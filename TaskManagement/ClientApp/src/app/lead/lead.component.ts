0e60f27ca58fcdb60f7336ec7b6c478f33da53d6
ravi <raviducktaleit@gmail.com>
2019-03-28 20:44:28 +0530

    UI

--- TaskManagement/ClientApp/src/app/lead/lead.component.ts
+++ TaskManagement/ClientApp/src/app/lead/lead.component.ts
@@ -7,10 +7,14 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class LeadComponent implements OnInit {
-
+  leads: any;
   constructor() { }
 
   ngOnInit() {
+    this.leads = [{ id: 1, name: "test1", description: "test sdf test description1", date: "01-01-2019" },
+      { id: 4, name: "test2", description: "test test2 sara description2", date: "01-05-2019" },
+      { id: 3, name: "test3", description: "test test356 dfytr description3", date: "02-01-2019" },
+      { id: 4, name: "test4", description: "test 4444 dfytr description3", date: "03-02-2019" }]
   }
 
 }
--- TaskManagement/ClientApp/src/app/lead/lead.module.ts
+++ TaskManagement/ClientApp/src/app/lead/lead.module.ts
@@ -4,15 +4,18 @@ import { RouterModule } from '@angular/router';
 import { LeadComponent } from './lead.component';
 import { LeadService } from './lead.service';
 import { AddEditLeadComponent } from './add-edit-lead/add-edit-lead.component';
+import { FormsModule } from '@angular/forms';
 
 export const routes = [
   { path: '', component: LeadComponent, pathMatch: 'full', data: { title: 'Manage Lead' } },
-  { path: 'add/:id', component: AddEditLeadComponent, pathMatch: 'full', data: { title: 'Invoice Information' } }
+  { path: 'add', component: AddEditLeadComponent, data: { title: 'Add' } },
+  { path: 'add/:id', component: AddEditLeadComponent, data: { title: 'Edit' } }
 ];
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule.forChild(routes),
   ],
   declarations: [
